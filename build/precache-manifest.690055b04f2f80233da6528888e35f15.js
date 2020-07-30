df553c53fc653275e72bbde92558886d7dc5ad2d
Nijat <nmursali2019@ada.edu.az>
2020-07-31 00:12:12 +0400

    git pages

---
+++ build/precache-manifest.690055b04f2f80233da6528888e35f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e19eb912bbb2f8ede2bc7d648aea8d",
+    "url": "/BlogApp-React/index.html"
+  },
+  {
+    "revision": "8cd4e13fcf448be0dd31",
+    "url": "/BlogApp-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "851ac5daf9caff0a1778",
+    "url": "/BlogApp-React/static/js/2.92fabb4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BlogApp-React/static/js/2.92fabb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd4e13fcf448be0dd31",
+    "url": "/BlogApp-React/static/js/main.3dd95419.chunk.js"
+  },
+  {
+    "revision": "cc9b4d3066082799e239",
+    "url": "/BlogApp-React/static/js/runtime-main.007d04fe.js"
+  }
+]);
